36fdfe0b5aa6e7cc34076f20d5e24bf35575d764
Andrew Eliyah <AndrewEliyah@icloud.com>
2017-12-22 22:34:28 -0800

    Installed AngularJs

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const del=require('del');
 const runSequence=require('run-sequence');
 
 // >Clean
-gulp.task('clean',function() {
+gulp.task('clean',function(){
   return del(['dev-build']);
 });
 
@@ -14,11 +14,16 @@ gulp.task('dev',function(){
   gulp.src(['src/header.html', 'src/body.html', 'src/footer.html'])
   .pipe(concat('index.html'))
   .pipe(gulp.dest('dev-build'));
+
+  // >>Gather Angular
+  gulp.src('node_modules/angular/angular.js')
+  .pipe(gulp.dest('dev-build/static/lib/js/'));
+  gulp.src('node_modules/angular-route/angular-route.js')
+  .pipe(gulp.dest('dev-build/static/lib/js/'));
 });
 
 // >dev-build
-gulp.task('dev-build',function(cb)
-{
+gulp.task('dev-build',function(cb){
   runSequence('clean',['dev'],cb);
 });
 
