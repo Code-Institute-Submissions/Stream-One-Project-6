0b670a22a6b7f14c338d5b095180a53b4f4a2b21
Andrew Eliyah <AndrewEliyah@icloud.com>
2017-12-29 19:26:48 -0800

    Created music section
    Created a music section with audio player

--- src/app/logic/controller.js
+++ src/app/logic/controller.js
@@ -1,17 +1,118 @@
-app.controller('navbar',function($scope, serverData){
-  // Generate site wide nav links
+app.controller("navbar",function($scope, serverData){
+  // >Generate site wide nav links
   serverData.getLink().then(function(data){
+    $scope.data=data.data;
+  },function(err){
+    console.error(err);
+  });
+});
+
+app.controller("memberList",function($scope, serverData){
+  // >Generate member list
+  serverData.getMember().then(function(data){
     $scope.data=data.data;
   },function(err){
     console.log(err);
   });
 });
 
-app.controller('memberList',function($scope, serverData){
-  // Generate member list
-  serverData.getMember().then(function(data){
+app.controller("audioCtl",function($scope, serverData,jukebox){
+  // >Generate audio list
+  serverData
+  .getAudio()
+  .then(function(data){
     $scope.data=data.data;
+
+    // >>Set Audio Player
+    jukebox.set(data.data,finished);
+
+    // >>Playback control
+    $scope.playback=function(id){
+      if(id==undefined){
+        console.error('Playback source undefined (needs id)');
+        return;
+      } 
+
+      switch(status(id)){
+        case "playing":
+          jukebox.playpause();
+          controllUpdate("play");
+        break;
+        case "paused":
+          jukebox.playpause();
+          controllUpdate("pause");
+        break;
+        case "ready":
+          jukebox.playpause(id);
+          controllUpdate("pause");
+          albumUIUpdate("show",id,$scope.data[id].title);
+        break;
+      }
+    }
+
+    // >>Set playback Control UI
+    function controllUpdate(p){
+      $(".music-player .glyphicon")
+      .removeClass("glyphicon-pause")
+      .removeClass("glyphicon-play");
+
+      switch(p){
+        case "pause":
+          $(".music-player .glyphicon")
+          .addClass("glyphicon-pause");
+        break;
+        case "play":
+          $(".music-player .glyphicon")
+          .addClass("glyphicon-play");
+        break;
+      }      
+    }
+
+    // >>Set Album UI
+    function albumUIUpdate(state,id,title){
+      switch(state){
+        case "hide":
+          $(".music-player .cover-art")
+          .slideUp(function(){
+            $(this).attr("src", "");
+          });
+
+          $(".music-player h3")
+          .slideUp();
+
+          $scope.playing="";
+        break;
+        case "show":
+          $(".music-player .cover-art")
+          .attr("src", $scope.data[id].cover)
+          .slideDown();
+
+          $scope.playing=title;
+
+          $(".music-player h3")
+          .slideDown();
+        break;
+      }
+    }
+
+    // >>Get player status
+    function status(id){
+      if(id!==undefined)
+      {
+        return "ready";
+      }
+      return jukebox.status();
+    }
+
+    // >>When playback finishes...
+    function finished(){
+      $(".music-player .glyphicon")
+      .removeClass("glyphicon-pause")
+      .removeClass("glyphicon-play");
+      controllUpdate("ready");
+      albumUIUpdate("hide");
+    }
   },function(err){
-    console.log(err);
+    console.error(err);
   });
 });
--- src/app/logic/factory.js
+++ src/app/logic/factory.js
@@ -1,13 +1,79 @@
-app.factory('serverData',function($http){
+app.factory("serverData",function($http){
 
   function getLink() {
-    return $http.get('data/linkData.json');
+    return $http.get("data/linkData.json");
   }
   function getMember() {
-    return $http.get('data/memberData.json');
+    return $http.get("data/memberData.json");
+  }
+  function getAudio() {
+    return $http.get("data/audioData.json");
   }
   return {
     getLink: getLink,
     getMember: getMember,
+    getAudio: getAudio
   }
-})
+});
+
+app.factory("jukebox", function(){
+  
+  var jukebox=document.createElement("AUDIO");
+  var data;
+  var status=null;
+  var callback;
+  // Reset jubox when song ends
+  jukebox.addEventListener("ended", function(){
+    jukebox.src="";
+    status="ready";
+    callback();
+  });
+
+  function set(music,cb){
+    data=music;
+    status="ready";
+    callback=cb;
+  }
+  function play(){
+    jukebox.play();
+    status="playing";
+  }
+  function pause(){
+    jukebox.pause();
+    status="paused";
+  }
+  function getStatus(){
+    return status;
+  }
+  function playpause(id){
+    if(id==undefined&&status=="ready"){
+      console.error("Error: 'playpause(id)' method requires json position ID of audio");
+    }
+    else if(id!==undefined){
+      // >>>Load Song
+      jukebox.src=data[id].src;
+      // >>>Play Song
+      play();
+    }
+    else{
+      switch(status)
+      {
+        case "playing":
+          pause();
+        break;
+        case "paused":
+          play();
+        break;
+        default:
+          console.error("Error: null status: Use 'set()' method to add json data.")
+        break;
+      }
+    }
+  }
+
+  return {
+    set: set,
+    playpause: playpause,
+    status: getStatus
+  }
+});
