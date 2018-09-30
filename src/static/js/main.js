3cdaa96e4d9e654b99a77ffea32bde32fbd20c09
Andrew Eliyah <andreweliyah@icloud.com>
2018-09-30 14:24:40 -0700

    The mobile navigation menu close on click
    The mobile navigation menu now closes whenever a link is clicked. So that it won't block the rest of the page.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -3,5 +3,9 @@ $(document).ready(function(){
   $('#loader').css('height','110%').delay(900).fadeOut(2000,function(){
     $('body').css('overflow','unset');
   });
+
+  $('.navbar-collapse.collapse').click(function(e){
+    $('.navbar-toggle').click();
+  })
 });
   
